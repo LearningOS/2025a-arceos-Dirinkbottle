c0df3943bbcc1f7da183e257ff9124ead18b17a6
Dirinkbottle <2909128143@qq.com>
2025-11-06 12:52:13 +0800

    2>dev/null

--- scripts/test-simple_hv.sh
+++ scripts/test-simple_hv.sh
@@ -16,8 +16,8 @@ make payload
 ./update_disk.sh payload/skernel2/skernel2
 
 # Create a helper script to feed periodic input to work around QEMU hang in pipe mode
-#qemu是事件驱动型，qemu会默认认为没有输入就不会去检查其它设备（没有其它事件的情况）。导致卡死.... 目前这个也有几率卡死
- (while true; do echo " "; sleep 0.05; done) |  make run A=exercises/simple_hv/ BLK=y 2>/dev/null | tee $tmp_file
+
+ (while true; do echo " "; sleep 0.05; done) 2>/dev/null |  make run A=exercises/simple_hv/ BLK=y 2>/dev/null | tee $tmp_file
 
 output=$(grep -a "$grep_content" $tmp_file | tail -n1  )
 
