329c2f86b3f49a46810c57b5fe344ff62957ab3a
dirinkbottle <122192254+Dirinkbottle@users.noreply.github.com>
2025-11-06 13:09:05 +0800

    pipe

--- scripts/test-simple_hv.sh
+++ scripts/test-simple_hv.sh
@@ -17,10 +17,10 @@ make payload
 
 # Create a helper script to feed periodic input to work around QEMU hang in pipe mode
 
- timeout 60 bash -c '(while true; do printf " \n"; sleep 0.1; done) 2>/dev/null | make run A=exercises/simple_hv/ BLK=y 2>/dev/null' | tee $tmp_file
+output_content=$(timeout 60 bash -c '(while true; do printf " \n"; sleep 0.1; done) 2>/dev/null | make run A=exercises/simple_hv/ BLK=y 2>/dev/null')
+echo "$output_content" | tee $tmp_file
 
-
-output=$(grep -a "$grep_content" $tmp_file | tail -n1  )
+output=$(echo "$output_content" | grep -a "$grep_content" | tail -n1)
 
 rm -rf $tmp_file 
 
